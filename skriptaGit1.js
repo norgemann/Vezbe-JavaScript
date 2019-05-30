82be62e7c98cea58e9d5350a796a02198029669e
norgemann <46277469+norgemann@users.noreply.github.com>
2019-05-30 12:27:46 +0200

    Update skriptaGit1.js

--- skriptaGit1.js
+++ skriptaGit1.js
@@ -1,6 +1,6 @@
 var noge = ["zmija", "covek bez jedne noge", "covek", "covek sa stapom", "pas"];
 var nasumicniIndex = Math.floor(Math.random()*noge.length);
 var nasumicnaElement = noge[nasumicniIndex];
-var poruka = prompt("koliko noga ima "+ nasumicnaElement+"?");
+var poruka = prompt("Koliko noga ima "+ nasumicnaElement+"?");
 var odgovor = (poruka == nasumicniIndex)? "Tacno!":"Netacno, " +nasumicnaElement+ " ima " + nasumicniIndex+ " noge.";
 alert(odgovor);
