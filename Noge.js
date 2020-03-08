99fd154ed4b79e60235a01481e6bc7fbb23256c4
norgemann <46277469+norgemann@users.noreply.github.com>
2020-03-08 08:09:45 +0100

    Pocinjemo Git

--- Noge.js
+++ Noge.js
@@ -1,6 +1,9 @@
 var noge = ["zmija", "covek bez jedne noge", "covek", "covek sa stapom", "pas"];
-var nasumicniIndex = Math.floor(Math.random()*noge.length);
-var nasumicnaElement = noge[nasumicniIndex];
-var poruka = prompt("Koliko noga ima "+ nasumicnaElement+"?");
-var odgovor = (poruka == nasumicniIndex)? "Tacno!":"Netacno, " +nasumicnaElement+ " ima " + nasumicniIndex+ " noge.";
+var nasumicniIndex = Math.floor(Math.random() * noge.length);
+var nasumicniElement = noge[nasumicniIndex];
+var poruka = prompt("Koliko noga ima " + nasumicniElement + "?");
+var odgovor =
+  poruka == nasumicniIndex
+    ? "Tacno!"
+    : "Netacno, " + nasumicnaElement + " ima " + nasumicniIndex + " noge.";
 alert(odgovor);
--- papirKamenMakaze.js
+++ papirKamenMakaze.js
@@ -1,24 +1,32 @@
 //**********PAPIR KAMEN MAKAZE*********************
 
 var pobeda = false;
-var igrac = prompt("Da li biras papir, kamen ili makaze?")
-var io = igrac.charAt(0).toUpperCase();//igracev odabir
+var igrac = prompt("Da li biras papir, kamen ili makaze?");
+var io = igrac.charAt(0).toUpperCase(); //igracev odabir
 var komp = Math.random();
-if(komp < 0.34) komp = "papir";
-else if(komp < 0.67) komp = "kamen";
+if (komp < 0.34) komp = "papir";
+else if (komp < 0.67) komp = "kamen";
 else komp = "makaze";
-var ko = komp.charAt(0).toUpperCase();//kompjuterov odabir
+var ko = komp.charAt(0).toUpperCase(); //kompjuterov odabir
 alert("Kompjuter: " + komp);
-console.log(io==ko);
+console.log(io == ko);
 
-if(io==ko){pobeda = "nereseno";}
-else if((io=='P'&& ko=='K')||(io=='K'&& ko=='M')||(io=='M'&& ko=='P'))pobeda = true; 
+if (io == ko) {
+  pobeda = "nereseno";
+} else if (
+  (io == "P" && ko == "K") ||
+  (io == "K" && ko == "M") ||
+  (io == "M" && ko == "P")
+)
+  pobeda = true;
 else;
-switch (pobeda)
-    {
-    case false: alert("Izgubio si.");
-            break;
-    case true: alert("Pobedio si.");
-            break;
-        default: alert("Nereseno.");
-    }
+switch (pobeda) {
+  case false:
+    alert("Izgubio si.");
+    break;
+  case true:
+    alert("Pobedio si.");
+    break;
+  default:
+    alert("Nereseno.");
+}
